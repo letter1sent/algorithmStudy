e68ec16d4bea295c35ca269443e930fc51fe2d3e
letter <letter1sent@gmail.com>
2023-06-29 13:47:26 +0900

    level01 step01

--- src/baekjoon/level_01/Step01_2557.java
+++ src/baekjoon/level_01/Step01_2557.java
@@ -1,5 +1,17 @@
 package baekjoon.level_01;
 
 public class Step01_2557 {
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
+
+/*
+public class Main {
+    public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+    }
+}
+
+*/
